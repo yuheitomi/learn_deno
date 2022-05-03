13b582f2adb4bf583bf76d3abfdd20476ad2dffb
Yuhei Kuratomi <yuhei.kuratomi@gmail.com>
2022-05-03 13:41:59 +0900

    update

--- native_hello_name.ts
+++ native_hello_name.ts
@@ -1,4 +1,7 @@
 const listener = Deno.listen({port: 18888})
 
+function serv() => undefined {
+  console.warn("warn");
+}
 
 
--- test.ts
+++ test.ts
@@ -1,12 +1,14 @@
-import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
-
-const addr = "18888";
-console.log(`HTTP server listening on http://localhost${addr}`);
+import { serve } from "https://deno.land/std@0.137.0/http/mod.ts";
 
 function handler(req: Request): Response {
   const pathname = new URL(req.url);
   return new Response(`Hello world from ${pathname}`);
 }
 
-serve(handler, { port: 18888 });
+function run_server() {
+  const port = 18888;
+  console.log(`HTTP server listening on http://localhost:${port}`);
+  serve(handler, { port: 18888 });
+}
 
+run_server();
